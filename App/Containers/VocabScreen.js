35258bad15794039a9f9024d86d37570755e45d5
Tyler Kirby <joseph.kirby12@ncf.edu>
2017-06-06 18:14:21 -0500

    added chapter select to vocab screen

--- App/Containers/VocabScreen.js
+++ App/Containers/VocabScreen.js
@@ -1,14 +1,30 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Picker } from 'react-native';
 import RoundedButton from '../Components/RoundedButton';
 
 // Styles
 import styles from './Styles/VocabScreenStyles'
 
 export default class VocabScreen extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {selectedChapter: 1};
+  }
+
+
+
   render() {
     return(
       <View style={styles.mainContainer}>
+        <View>
+          <Picker
+            style={{width: 100}}
+            selectedValue={this.state.selectedChapter}
+            onValueChange={(itemValue, itemIndex) => this.setState({selectedChapter: itemValue})}>
+            <Picker.Item label="Chapter 1" value={1} />
+            <Picker.Item label="Chapter 2" value={2} />
+          </Picker>
+        </View>
         <View style={styles.cardContainer}>
           <Text style={styles.cardText}>Cards go here</Text>
         </View>
