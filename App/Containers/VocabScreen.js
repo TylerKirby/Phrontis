22839f241d2d91ac698dbfbc781115d5b3428fe0
Tyler Kirby <joseph.kirby12@ncf.edu>
2017-06-15 22:24:42 -0500

    started showVocab method

--- App/Containers/VocabScreen.js
+++ App/Containers/VocabScreen.js
@@ -1,14 +1,34 @@
 import React from 'react';
 import { View, Text, Picker } from 'react-native';
 import RoundedButton from '../Components/RoundedButton';
+import unit1 from '../../GreekAppData/unit1.json';
 
 // Styles
 import styles from './Styles/VocabScreenStyles'
+
+console.log(unit1);
+
+const vocabData = [{unitNumber: 1, dataFile: unit1}];
 
 export default class VocabScreen extends React.Component {
   constructor(props) {
     super(props);
     this.state = {selectedChapter: 1};
+
+    this.onChapterChange = this.onChapterChange.bind(this);
+    this.showVocab = this.showVocab.bind(this);
+  }
+
+  onChapterChange(itemValue) {
+    this.setState({
+      selectedChapter: itemValue
+    });
+  }
+
+  showVocab() {
+    const chapterData = vocabData.find(unit => unit.unitNumber == 1).dataFile;
+    console.log(chapterData);
+    return Object.keys(chapterData['vocab'][0])[0]
   }
 
 
@@ -24,12 +44,12 @@ export default class VocabScreen extends React.Component {
           <Picker
             style={{width: 100}}
             selectedValue={this.state.selectedChapter}
-            onValueChange={(itemValue, itemIndex) => this.setState({selectedChapter: itemValue}, console.log(this.state))}>
+            onValueChange={this.onChapterChange}>
             {unitsButtons}
           </Picker>
         </View>
         <View style={styles.cardContainer}>
-          <Text style={styles.cardText}>Cards go here</Text>
+          <Text style={styles.cardText}>{this.showVocab()}</Text>
         </View>
         <View style={styles.buttonContainer}>
           <RoundedButton text="✔️"/>
